04219a7e9ff0a66439ef2b1d1d810e56e2ca5b2c
Nil Folquer <nfolquer@gmail.com>
2019-11-20 21:58:41 +0100

    changed gitignore

--- index.js
+++ index.js
@@ -13,6 +13,19 @@ client.on('ready', () => {
       let tweet = await getTweet();
       msg.reply(`Bertin ha twiteado: \n ${tweet.text} \n ${tweet.img}`);
     }
+    
   });
+
+  client.on('message', async (msg) => {
+    console.log(msg.content);
+    if (msg.content === 'españa') {
+      console.log('in')
+      const img = new Discord.Attachment('./img/espanita.jpeg', 'espanita');
+      msg.reply('Españita', {files: img,});
+    }
+    
+  });
+ 
+
   
   client.login(process.env.DISCORD_TOKEN);
