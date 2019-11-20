827d652e89e48aba95f1172a6bc8ab22ce8b32d7
Ubuntu <ubuntu@ip-172-31-27-55.eu-west-3.compute.internal>
2019-11-20 20:56:07 +0000

    Added the soy programador response

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client()
 const getTweet = require('./scrap')
+const ytdl = require('ytdl-core');
 
 require('dotenv').config()
+
+let gif_programador = 'https://thumbs.gfycat.com/MildVigilantAntarcticgiantpetrel-size_restricted.gif'
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -12,7 +15,22 @@ client.on('ready', () => {
     if (msg.content === 'bertin') {
       let tweet = await getTweet();
       msg.reply(`Bertin ha twiteado: \n ${tweet.text} \n ${tweet.img}`);
+    } else if(msg.content.includes('programador')) {
+	msg.reply(`Soy Programador: ${gif_programador}`);
+    } else if(msg.content.includes('-canta')) {
+	msg.channel.send('-play como un vagabundo');
     }
   });
-  
+const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+ .on('end', () => {
+  console.log('Music ended!');
+  // Deletes the finished song from the queue
+  serverQueue.songs.shift();
+  // Calls the play function again with the next song
+  play(guild, serverQueue.songs[0]);
+})
+ .on('error', error => {
+  console.error(error);
+ });
+dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);  
   client.login(process.env.DISCORD_TOKEN);
