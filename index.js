1a9663eaa316a8126c5e614b4a2265e1bc10bbb4
Nil Folquer <nfolquer@gmail.com>
2019-11-20 22:28:03 +0100

    Deleted modules

--- index.js
+++ index.js
@@ -7,32 +7,29 @@ require('dotenv').config()
 
 let gif_programador = 'https://thumbs.gfycat.com/MildVigilantAntarcticgiantpetrel-size_restricted.gif'
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-  });
+function play() {
+  let title = 'song.mp3'
   
-  client.on('message', async (msg) => {
-    if (msg.content.toLower() === 'bertin') {
-      let tweet = await getTweet();
-      msg.reply(`Bertin ha twiteado: \n ${tweet.text} \n ${tweet.img}`);
-    } else if(msg.content.includes('programador')) {
-	msg.reply(`Soy Programador: ${gif_programador}`);
-    } else if(msg.content.includes('-canta')) {
-	msg.channel.send('-play como un vagabundo');
-    }
-    
-  });
-<<<<<<< HEAD
-const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
- .on('end', () => {
-  console.log('Music ended!');
-  // Deletes the finished song from the queue
-  serverQueue.songs.shift();
-  // Calls the play function again with the next song
-  play(guild, serverQueue.songs[0]);
-})
- .on('error', error => {
-  console.error(error);
- });
-dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);  
-  client.login(process.env.DISCORD_TOKEN);
+}
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', async (msg) => {
+  if (msg.content.toLower() === 'bertin') {
+    let tweet = await getTweet();
+    msg.reply(`Bertin ha twiteado: \n ${tweet.text} \n ${tweet.img}`);
+  } else if (msg.content.includes('programador')) {
+    msg.reply(`Soy Programador: ${gif_programador}`);
+  } else if (msg.content.includes('-canta')) {
+    play();
+  }
+
+});
+
+
+
+
+client.login(process.env.DISCORD_TOKEN);
