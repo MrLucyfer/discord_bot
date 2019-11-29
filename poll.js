98618feb0a75e80afa4e323261d6b25f51c22514
Ubuntu <ubuntu@ip-172-31-27-55.eu-west-3.compute.internal>
2019-11-29 16:32:37 +0000

    Poll.js made

--- poll.js
+++ poll.js
@@ -31,7 +31,7 @@ function referendum(msg) {
     if (msg.member.roles.has(guildID)) {
         msg.react('👍').then(() => msg.react('👎'));
         voted = [];
-        const collector = msg.createReactionCollector(filter, { time: 1000 });
+        const collector = msg.createReactionCollector(filter, { time: 600000 });
         collector.on('end', collected => {
             let results = parseResults(collected);
             msg.channel.send(`@everyone Final de la votación! \nVotos totales: ${collected.size} \nResultado: 👍 -> ${results[0]}  ------  👎 -> ${results[1]}`)
